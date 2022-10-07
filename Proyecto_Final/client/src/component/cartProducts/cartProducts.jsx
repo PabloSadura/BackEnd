c38cf9bdee4c1aadc228ba93f8fb87d1f2bdb518
PabloSadura <pablosadura@gmail.com>
2022-10-07 16:08:58 -0300

    cambios en client

--- Proyecto_Final/client/src/component/cartProducts/cartProducts.jsx
+++ Proyecto_Final/client/src/component/cartProducts/cartProducts.jsx
@@ -1,15 +1,15 @@
 import Card from "react-bootstrap/Card";
 
 function CardProduct({ products }) {
-  const { id, nombre, precio, codigo, stock, descripcion } = products;
+  const { id, name, price, code, stock, description } = products;
   return (
     <Card style={{ width: "18rem" }} className="mt-4">
       <Card.Body>
         <Card.Title>
-          #{id} {nombre}
+          #{id} {name}
         </Card.Title>
-        <Card.Text>{descripcion}</Card.Text>
-        <Card.Text>$ {precio}</Card.Text>
+        <Card.Text>{description}</Card.Text>
+        <Card.Text>$ {price}</Card.Text>
         <Card.Text>Stock: {stock}</Card.Text>
         <Card.Link href="">Comprar</Card.Link>
       </Card.Body>
--- Proyecto_Final/server/routes/cartRoutes.js
+++ Proyecto_Final/server/routes/cartRoutes.js
@@ -6,80 +6,18 @@ let products = [];
 let cart = [];
 let idCart = 1;
 
-(() => {
-  fs.promises.readFile("products.txt", "utf-8").then((response) => {
-    const data = JSON.parse(response);
-    products.push(...data);
-  });
-})();
-
-(() => {
-  fs.promises.readFile("cart.txt", "utf-8").then((response) => {
-    if (response) {
-      const data = JSON.parse(response);
-      cart.push(...data);
-    }
-  });
-})();
-
-const save = (file, array) => {
-  fs.promises.writeFile(file, JSON.stringify(array));
-};
-
 // lista todos los productos del carrito
-cartRouter.get("/:id/products", (req, res) => {
-  const { id } = req.params;
-  const data = cart.find((el) => el.id === Number(id));
-  res.json(Date());
-  res.json(data);
-});
+cartRouter.get("/:id/products", (req, res) => {});
 
 //  Crea un carrito y devuelve un id
-cartRouter.post("/", (req, res) => {
-  cart.push({ id: idCart, products: [] });
-  res.json({ mensaje: "Carrito creado con exito", id: idCart });
-  idCart++;
-});
+cartRouter.post("/", (req, res) => {});
 
 // Vacía un carrito y lo elimina
-cartRouter.delete("/:idCart", (req, res) => {
-  const { idCart } = req.params;
-  cart = [];
-  save("cart.txt", cart);
-  res.json({ mensaje: "Se Elimino correctamente el carrito" });
-});
+cartRouter.delete("/:idCart", (req, res) => {});
 
 // Para incorporar productos al carrito por su id de producto
-cartRouter.post("/:idCart/:id/products", (req, res) => {
-  const { id } = req.params;
-  const { idCart } = req.params;
-  try {
-    const data = products.find((el) => el.id === Number(id));
-    const dataCart = cart.find((el) => el.id === Number(idCart));
-    const { products: product } = dataCart;
-    product.push(data);
-    save("cart.txt", cart);
-    res.json({ mensaje: "Se agrego correctamente el producto" });
-  } catch (error) {
-    console.log("Error: ", error);
-  }
-});
+cartRouter.post("/:idCart/:id/products", (req, res) => {});
 
 //  Eliminar un producto del carrito por su id de carrito y de producto
-cartRouter.delete("/:idCart/:id_prod/products", (req, res) => {
-  const { idCart } = req.params;
-  const { id_prod } = req.params;
-  try {
-    const dataCart = cart.find((el) => el.id === Number(idCart));
-    const { products: product } = dataCart;
-    const data = product.findIndex((el) => el.id === Number(id_prod));
-    product.splice(data, 1);
-    save("cart.txt", cart);
-    res.json({ mensaje: "Se elimino correctamente el producto" });
-
-    console.log(cart);
-  } catch (error) {
-    console.log("Error: ", error);
-  }
-});
+cartRouter.delete("/:idCart/:id_prod/products", (req, res) => {});
 export default cartRouter;
