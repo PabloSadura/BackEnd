e9fef1f5867489b946c1fb37ba712fdc5d855808
PabloSadura <pablosadura@gmail.com>
2023-02-14 15:46:16 -0300

    Se reinicio el proyecto a 0

--- persistencia/dbConfig.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-mongoose
-  .connect(
-    "mongodb+srv://ecommerce:coderhouse@cluster0.qrpyisw.mongodb.net/ecommerce?retryWrites=true&w=majority"
-  )
-  .then((db) => console.log("Base de datos conectada"))
-  .catch((error) => console.log(error));
--- services/emailServices.js
+++
@@ -1,7 +0,0 @@
-import { Email } from "../persistencia/contenedores/emailClass";
-
-const emailCart = new Email();
-
-export async function sendMail(user, mail, product) {
-  emailCart.emailCart(user, mail, product);
-}
